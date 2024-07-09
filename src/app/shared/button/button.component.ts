e5784b4cd9ce1d8ea9b8aaa4c6a97bfd524567ac
Mantas Urbonas <mantas.urbonas@idenfy.com>
2024-07-09 12:15:56 +0300

    targetting host element

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -6,6 +6,6 @@ import { Component, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.css',
-  encapsulation: ViewEncapsulation.None,
+  // encapsulation: ViewEncapsulation.None,
 })
 export class ButtonComponent {}
