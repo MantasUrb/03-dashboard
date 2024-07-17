e87f0bbad0737a3b751b7a68cc412bc864f5ca6e
Mantas Urbonas <mantas.urbonas@idenfy.com>
2024-07-17 16:00:13 +0300

    cleaning up internval with nhOnDestroy

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,12 +7,14 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent implements OnInit {
-
+export class ServerStatusComponent implements OnInit, OnDestroy {
   currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  private interval?: ReturnType<typeof setInterval>;
 
   ngOnInit() {
-    setInterval(() => {
+    console.log('ON INIT');
+
+    this.interval = setInterval(() => {
       const rnd = Math.random(); // 0 - 0.9999999999
 
       if (rnd < 0.5) {
@@ -23,5 +25,13 @@ export class ServerStatusComponent implements OnInit {
         this.currentStatus = 'unknown';
       }
     }, 5000);
+  }
+
+  ngAfterViewInit() {
+    console.log('AFTER VIEW INIT');
+  }
+
+  ngOnDestroy() {
+    clearTimeout(this.interval);
   }
 }
